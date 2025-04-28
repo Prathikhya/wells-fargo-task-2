23099c8729cf5492188cebb1faa47f4370dc4300
prathikhya <prathikhyadevi25@gmail.com>
2025-04-28 23:51:47 +0530

    "commited again"

--- src/main/java/com/wellsfargo/counselor/entity/Client.java
+++ src/main/java/com/wellsfargo/counselor/entity/Client.java
@@ -23,7 +23,7 @@ public class Client {
     private FinancialAdvisor1 financialAdvisor;
 
     // One-to-one relationship with Portfolio
-    @OneToOne(mappedBy = "clients")
+    @OneToOne(mappedBy = "client")
     private Portfolio portfolio;
 
     // Constructor
